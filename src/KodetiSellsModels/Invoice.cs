cbcbbe4a0b1b936d2c7482057df62b51e6737f14
emilio barrera <barrera_emilio@hotmail.com>
2019-12-06 00:15:46 +0000

    Add project files.

---
+++ src/KodetiSellsModels/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KodetiSellsModels
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public decimal Iva { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+
+        //relation
+        public Client Client { get; set; }
+    }
+}
